2677fcec9e36964700c4f9fdc938215c3b7f66fb
Livio Alvarenga <livioalvarenga@gmail.com>
2022-11-22 17:34:08 -0300

    add componente banner

---
+++ src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import banner from "../assets/images/banner.png";
+export default function Banner() {
+  return (
+    <div className="relative w-[88%]">
+      <h1 className="absolute top-[75px] left-11 h-36 w-[350px] font-gandhiSerif text-5xl text-white">
+        A galeria mais completa de fotos do espaço!
+      </h1>
+      <img className="h-72 w-full" src={banner} alt="A imagem da terra vista do espaço" />
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 import Menu from "../components/Menu";
 
@@ -5,7 +6,12 @@ export default function Home() {
   return (
     <>
       <Header />
-      <Menu />
+      <main className="my-0 mx-6">
+        <section className="mt-8 flex items-center justify-between">
+          <Menu />
+          <Banner />
+        </section>
+      </main>
     </>
   );
 }
