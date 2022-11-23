6bf05b63748147461ba7f9d859518f15583bf439
Livio Alvarenga <livioalvarenga@gmail.com>
2022-11-23 11:12:57 -0300

    Eliminando codigo repetido em tags

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import tags from "../data/tags.json";
 
 export default function Tags() {
   return (
     <div className="flex items-center justify-between">
       <p className="text-space-100">Busque por tags:</p>
       <ul className="flex items-center justify-between gap-5 p-0 text-white">
-        <li className="cursor-pointer rounded-lg bg-space-800 py-2 px-3">Estrelas</li>
-        <li className="cursor-pointer rounded-lg bg-space-800 py-2 px-3">Galáxias</li>
-        <li className="cursor-pointer rounded-lg bg-space-800 py-2 px-3">Lua</li>
-        <li className="cursor-pointer rounded-lg bg-space-800 py-2 px-3">Planetas</li>
+        {tags.map((tag) => {
+          return (
+            <li
+              className="cursor-pointer rounded-lg bg-space-800 py-2 px-3 duration-500 ease-in-out hover:bg-gradient-to-r hover:from-space-300 hover:to-space-400"
+              key={tag.id}
+            >
+              {tag.text}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
