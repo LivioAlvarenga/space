93f9a4fdb56b52f88eb7ea0c71cde41183840000
Livio Alvarenga <livioalvarenga@gmail.com>
2022-11-23 15:51:50 -0300

    add state de filtro por tags

--- src/components/Galeria.jsx
+++ src/components/Galeria.jsx
@@ -1,12 +1,31 @@
-import cards from "data/photos.json";
 import Cards from "components/Cards";
 import Tags from "components/Tags";
+import dataCards from "data/photos.json";
+import { useState } from "react";
 
 export default function Galeria() {
+  const [cards, setCards] = useState(dataCards);
+
+  // Gerando uma lista de tags
+  const tags = [...new Set(dataCards.map((item) => item.tag))].sort();
+
+  // Add os cards filtrados por tag
+  const filterCardsFunction = (tag) => {
+    const newCards = dataCards.filter((card) => {
+      return card.tag === tag;
+    });
+    setCards(newCards);
+  };
+
   return (
     <section className="mb-8 w-[65vw]">
       <h2 className="mb-7 text-3xl text-space-300">Navegue pela galeria</h2>
-      <Tags />
+      <Tags
+        tags={tags}
+        filterCards={filterCardsFunction}
+        setCards={setCards}
+        dataCards={dataCards}
+      />
       <Cards cards={cards} />
     </section>
   );
--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -1,20 +1,25 @@
-import tags from "data/tags.json";
-
-export default function Tags() {
+export default function Tags(props) {
   return (
     <div className="flex items-center justify-between">
       <p className="text-space-100">Busque por tags:</p>
       <ul className="flex items-center justify-between gap-5 p-0 text-white">
-        {tags.map((tag) => {
+        {props.tags.map((tag) => {
           return (
             <li
-              className="cursor-pointer rounded-lg bg-space-800 py-2 px-3 duration-500 ease-in-out hover:bg-gradient-to-r hover:from-space-300 hover:to-space-400"
-              key={tag.id}
+              className="relative cursor-pointer rounded-lg bg-space-800 py-2 px-3 duration-500 ease-in-out hover:bg-gradient-to-r hover:from-space-300 hover:to-space-400"
+              key={tag}
+              onClick={() => props.filterCards(tag)}
             >
-              {tag.text}
+              {tag}
             </li>
           );
         })}
+        <li
+          className="cursor-pointer rounded-lg bg-space-800 py-2 px-3 duration-500 ease-in-out hover:bg-gradient-to-r hover:from-space-300 hover:to-space-400"
+          onClick={() => props.setCards(props.dataCards)}
+        >
+          Todas
+        </li>
       </ul>
     </div>
   );
